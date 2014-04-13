9b46b6dd195c64596a4b23414813c033c5c72fb9
Paul Wenig <pwenig@gmail.com>
2014-04-13 12:33:19 -0600

    Rackup running. Index rendered

---
+++ config.ru
@@ -0,0 +1,6 @@
+require_relative 'song_app'
+require 'sequel'
+
+#database info to come
+
+run SongApp
--- song_app.rb
+++ song_app.rb
@@ -1,4 +1,8 @@
 require 'sinatra/base'
 class SongApp < Sinatra::Application
 
+  get '/' do
+
+    erb :index
+  end
 end
